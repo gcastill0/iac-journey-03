9512b678a374fef4e0dec2b3ca7dad18bde39dfa
gcastill0 <gilberto.castillo@rogers.com>
2023-10-24 22:26:50 -0400

    Updating diagram assets

--- docs/js/main.js
+++ docs/js/main.js
@@ -13,4 +13,64 @@ window.onload = function () {
     fetchSVG("img/topology.svg").then(imageSVG => {
         topology.appendChild(imageSVG.documentElement);
     });
+
+    let counter = 0;
+
+    const target_groups = ["region", "vpc", "az-all", "security-group", "ec2", "icon-alb", "users"]
+
+    function waitForSVGElement(selector) {
+        return new Promise(resolve => {
+            if (document.getElementById(selector)) {
+                return resolve(document.getElementById(selector));
+            }
+
+            const observer = new MutationObserver(mutations => {
+                if (document.getElementById(selector)) {
+                    observer.disconnect();
+                    resolve(document.getElementById(selector));
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+
+    waitForSVGElement(target_groups[counter]).then((element) => {
+        document.getElementById("aws").style.display = "block"
+    })
+
+    console.table(target_groups)
+
+    document.addEventListener('keydown', (e) => {
+
+        if (counter > target_groups.length - 1) {
+            console.log("from", counter, "to", target_groups.length - 1)
+            counter = target_groups.length - 1
+        } else if (counter <= 0) {
+            console.log("from", counter, "to", 0)
+            counter = 0
+        }
+
+        console.log("Current:", counter, target_groups[counter])
+
+        switch (e.key) {
+            case "ArrowRight":
+                // setActive(counter)
+                document.getElementById(target_groups[counter]).style.display = "block"
+                counter++
+                break
+            case "ArrowLeft":
+                // setInactive(counter)
+                document.getElementById(target_groups[counter]).style.display = "none"
+                counter--
+                break
+        }
+
+        console.log("Next:", counter, target_groups[counter], "\n")
+
+    })
+
 }
